d4632a23ba285d9d94ab6fc48e97d2c03ec63572
Hazar Gul Nazari <hazar.nazari@favendo.com>
2020-04-12 12:38:32 +0200

    changes

--- src/main/java/springconfigserver/demo/SpringConfigServerApplication.java
+++ src/main/java/springconfigserver/demo/SpringConfigServerApplication.java
@@ -1,9 +1,11 @@
-package springconfigserver.demo.springconfigserver;
+package springconfigserver.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringConfigServerApplication {
 
     public static void main(String[] args) {
